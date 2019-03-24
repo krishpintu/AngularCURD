be02e63b1f1a8da35c1f7f1488d81cfcc469df15
unknown <krishpintu@gmail.com>
2019-03-24 21:18:45 +0530

    nev header design and update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent }  from './home/home.component';
+import { EmployeeAddComponent }  from './employee/employee-add/employee-add.component';
+import { EmployeeListComponent }  from './employee/employee-list/employee-list.component';
 
 const routes: Routes = [
-  { path: 'ng-curd', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'ng-curd', component: HomeComponent },
+  { path: 'addemployee', component: EmployeeAddComponent },
+  { path: 'employees', component: EmployeeListComponent }
 ];
 
 @NgModule({
