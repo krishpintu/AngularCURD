ffd22932ce64b1003b1083207b2498f720451a6e
unknown <krishpintu@gmail.com>
2019-03-24 20:07:39 +0530

    home, header, employee-list and employee-add component created. [this commit for trust]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeAddComponent } from './employee/employee-add/employee-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    EmployeeListComponent,
+    EmployeeAddComponent
   ],
   imports: [
     BrowserModule,
